a3333896d9009da38757af3db8c9a19d78af386f
TristanBoulesteix <tristan.boulesteix@gmail.com>
2018-11-23 15:26:35 +0100

    Add classes to create a MVC structure

---
+++ Contract/src/main/java/jdr/exia/controler/IControler.java
@@ -0,0 +1,5 @@
+package jdr.exia.controler;
+
+public interface IControler {
+	public void start();
+}
--- main/src/main/java/jdr/exia/Olebo.java
+++ main/src/main/java/jdr/exia/Olebo.java
@@ -1,7 +1,18 @@
 package jdr.exia;
 
+import jdr.exia.controler.IControler;
+import jdr.exia.model.IModel;
+import jdr.exia.view.IView;
+
 public class Olebo {
 	public static void main(String[] args) {
+		// Instantiation of the MVC and start of the program
+		IModel model = new Model();
+		IView viewManager = new ViewManager();
+		IControler controler = new Controler(viewManager, model);
+		controler.start();
+
+		// Hello world !
 		System.out.println("Hello world !");
 	}
 }
