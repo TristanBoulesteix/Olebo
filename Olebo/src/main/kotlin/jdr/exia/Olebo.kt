ce2bcc4c4ff268751e742f57fec9bcff8e0bcdaa
TristanBoulesteix <tristan.boulesteix@gmail.com>
2019-10-02 17:00:57 +0200

    Update

--- Olebo/src/main/kotlin/jdr/exia/Olebo.kt
+++ Olebo/src/main/kotlin/jdr/exia/Olebo.kt
@@ -4,9 +4,9 @@ import jdr.exia.controller.Controller
 import jdr.exia.view.ViewManager
 
 fun main() {
-    val viewManager = ViewManager
-    val controller = Controller(viewManager)
-    controller.initDatas()
-    controller.start()
+    Controller(ViewManager).apply {
+        this.initDatas()
+        this.start()
+    }
 }
 
--- Olebo/src/main/kotlin/jdr/exia/controller/Controller.kt
+++ Olebo/src/main/kotlin/jdr/exia/controller/Controller.kt
@@ -1,9 +1,12 @@
 package jdr.exia.controller
 
+import jdr.exia.model.act.Act
+import jdr.exia.model.dao.DAO
 import jdr.exia.view.ViewManager
 
 class Controller(private val viewManager: ViewManager) {
-    fun initDatas() {
+    fun initDatas(): MutableList<Act> {
+        return DAO.getAllActs()
     }
 
     fun start() {
