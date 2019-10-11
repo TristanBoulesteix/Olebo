a77c02c26c51b9eec580a5758cbdcaf6222b2f5c
VincentOnfray <vincent.onfray@viacesi.fr>
2019-10-11 10:58:27 +0200

    Added SetSize to Element

--- Olebo/src/main/kotlin/jdr/exia/model/element/Element.kt
+++ Olebo/src/main/kotlin/jdr/exia/model/element/Element.kt
@@ -9,10 +9,17 @@ open class Element( //TODO return to abstract
     val sprite: ImageIcon,
     var position: Position,
     var visible: Boolean = false,
-    val size: Size,
+    size: Size,
     var hitBox: Rectangle = Rectangle(position.x,position.y,size.absoluteSizeValue,size.absoluteSizeValue)
 
 ){
+
+
+    var size:Size = size
+        set(newSize:Size){
+            field = newSize
+            hitBox = Rectangle(position.x,position.y,newSize.absoluteSizeValue,newSize.absoluteSizeValue)
+        }
     fun setPosition(x: Int,y: Int){
         this.position = Position(x,y)
         this.hitBox = Rectangle(x,y,size.absoluteSizeValue,size.absoluteSizeValue)
