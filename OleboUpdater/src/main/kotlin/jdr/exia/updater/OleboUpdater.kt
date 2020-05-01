247c1781ff5f15b9f69864103bf08bf97bc95be4
Tristan <tristan.boulesteix@gmail.com>
2020-05-01 18:50:00 +0200

    Updater tested

--- OleboUpdater/src/main/kotlin/jdr/exia/updater/OleboUpdater.kt
+++ OleboUpdater/src/main/kotlin/jdr/exia/updater/OleboUpdater.kt
@@ -1,13 +1,16 @@
 package jdr.exia.updater
 
 import java.awt.TrayIcon
-import java.io.*
+import java.io.File
+import java.io.InputStream
 import kotlin.system.exitProcess
 
 
 const val VERSION = "1.0.1"
 
 fun main(args: Array<String>) {
+    
+
     if (args.size == 1) println(VERSION)
 
     if (args.size != 2) exitProcess(-1)
