b2564a0749ab21356c5ceeade01d3a90c8b3f884
Tristan <tristan.boulesteix@gmail.com>
2020-11-05 18:31:36 +0100

    Optimisation of updater

--- Olebo/src/main/kotlin/jdr/exia/Olebo.kt
+++ Olebo/src/main/kotlin/jdr/exia/Olebo.kt
@@ -1,6 +1,6 @@
 package jdr.exia
 
-import jdr.exia.utils.HttpClientUpdater
+import jdr.exia.utils.checkForUpdate
 import jdr.exia.view.homeFrame.HomeFrame
 import javax.swing.SwingUtilities
 import javax.swing.UIManager
@@ -8,7 +8,7 @@ import javax.swing.UIManager
 const val VERSION = "1.1.1-BETA"
 
 fun main() {
-    HttpClientUpdater.checkForUpdate()
+    checkForUpdate()
     SwingUtilities.invokeLater {
         UIManager.setLookAndFeel(
             UIManager.getSystemLookAndFeelClassName()
