7539ddc1e0ef29cd1594285ad12231350e8218b6
Tristan <tristan.boulesteix@gmail.com>
2020-11-20 18:38:34 +0100

    Update ViewManager.kt

--- Olebo/src/main/kotlin/viewModel/ViewManager.kt
+++ Olebo/src/main/kotlin/viewModel/ViewManager.kt
@@ -51,8 +51,8 @@ object ViewManager {
     }
 
     private fun loadCurrentScene() {
-        with(activeAct) activeAct@{
-            activeScene = this!!.scenes.findWithId(this@activeAct.sceneId)
+        with(activeAct!!) activeAct@{
+            activeScene = this.scenes.findWithId(this@activeAct.sceneId)
             ViewFacade.apply {
                 this.loadItems()
                 this.setMapBackground(activeScene!!.background)
