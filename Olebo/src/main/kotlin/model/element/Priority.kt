d5b1e3e09aeae1f5d3873ddd9eeef337f96d8ece
Tristan <tristan.boulesteix@gmail.com>
2020-12-02 19:28:41 +0100

    Update Priority.kt

--- Olebo/src/main/kotlin/model/element/Priority.kt
+++ Olebo/src/main/kotlin/model/element/Priority.kt
@@ -7,18 +7,15 @@ import org.jetbrains.exposed.dao.EntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.sql.transactions.transaction
 
-enum class Priority(val priority: PriorityElement, val priorityName: String) {
-    LOW(transaction(DAO.database) { PriorityElement[1] }, "Arrière plan"),
-    REGULAR(transaction(DAO.database) { PriorityElement[2] }, "Défaut"),
-    HIGH(transaction(DAO.database) { PriorityElement[3] }, "Premier plan");
+enum class Priority(val priority: PriorityElement) {
+    LOW(transaction(DAO.database) { PriorityElement[1] }),
+    REGULAR(transaction(DAO.database) { PriorityElement[2] }),
+    HIGH(transaction(DAO.database) { PriorityElement[3] });
 
     class PriorityElement(id: EntityID<Int>) : Entity<Int>(id) {
         companion object : EntityClass<Int, PriorityElement>(PriorityTable)
 
         private val actualName by PriorityTable.priority
-
-        val name
-            get() = priorityElement.priorityName
 
         val priorityElement
             get() = when (this.actualName) {
