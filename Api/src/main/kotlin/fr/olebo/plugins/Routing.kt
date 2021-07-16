063b9c0b3e4bcd69a64152f44caafbf3ed380be9
Tristan <tristan.boulesteix@gmail.com>
2021-07-16 16:04:02 +0200

    Update Routing.kt

--- Api/src/main/kotlin/fr/olebo/plugins/Routing.kt
+++ Api/src/main/kotlin/fr/olebo/plugins/Routing.kt
@@ -2,6 +2,8 @@ package fr.olebo.plugins
 
 import fr.olebo.model.Version
 import io.ktor.application.*
+import io.ktor.http.content.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import java.io.File
@@ -12,8 +14,19 @@ fun Application.configureRouting() {
         get("/versions/last") {
             call.respond(Version(1, "Test"))
         }
-        get("/versions/{id}") {
+        get("/versions/{versionCode}") {
             call.respondFile(File(""))
+        }
+        put("/versions") {
+            val versionData = call.receive<Version>()
+
+            val fileData = call.receiveMultipart().readAllParts().firstOrNull()
+
+            if (fileData is PartData.FileItem) {
+                fileData.streamProvider().use {
+                    File("Olebo_${versionData.versionName}_${versionData.versionCode}").writeBytes(it.readBytes())
+                }
+            }
         }
     }
 }
