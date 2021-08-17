7c310a9aa3ba408dc21632a645336a4f6d3d1444
Tristan <tristan.boulesteix@gmail.com>
2021-08-17 12:44:57 +0200

    Update Release.kt

--- Website/src/main/kotlin/update/Release.kt
+++ Website/src/main/kotlin/update/Release.kt
@@ -3,9 +3,10 @@ package fr.olebo.update
 import jdr.exia.update.Release
 import java.io.File
 
-//const val OLEBO_RELEASES_DIRECTORY = "~/Olebo_releases"
+const val OLEBO_RELEASES_DIRECTORY = "~/Olebo_releases"
 
-val OLEBO_RELEASES_DIRECTORY = System.getProperty("user.dir") + File.separator + "test"
+// For debug purpose
+//val OLEBO_RELEASES_DIRECTORY = System.getProperty("user.dir") + File.separator + "test"
 
 val releases
     get() = ReleaseDirectory.getFromParent(File(OLEBO_RELEASES_DIRECTORY)).map { Release(it.versionCode, it.versionName, it.installerPaths) }.sorted()
