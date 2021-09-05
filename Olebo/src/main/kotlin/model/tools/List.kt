2a24366d228868f756af4618a1967afff247b309
Tristan <tristan.boulesteix@gmail.com>
2021-09-05 12:35:56 +0200

    Update List.kt

--- Olebo/src/main/kotlin/model/tools/List.kt
+++ Olebo/src/main/kotlin/model/tools/List.kt
@@ -3,4 +3,4 @@ package jdr.exia.model.tools
 import jdr.exia.model.element.Element
 
 inline fun <R> List<Element>.doIfContainsSingle(defaultValue: R, action: (Element) -> R) =
-    if (size == 1) action(this[0]) else defaultValue
+    if (size == 1) action(this.first()) else defaultValue
