bcd5b4906f69ceea4718f014069926656710caa8
Tristan <tristan.boulesteix@gmail.com>
2021-09-11 11:20:54 +0200

    Improve MutableState with custom setter

--- Olebo/src/main/kotlin/model/tools/State.kt
+++ Olebo/src/main/kotlin/model/tools/State.kt
@@ -1,20 +1,13 @@
 package jdr.exia.model.tools
 
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 
-inline infix fun <T> MutableState<T>.withSetter(crossinline setter: (newValue: T) -> Unit) = object : MutableState<T> {
-    private var delegatedValue by this@withSetter
-
-    override var value
-        get() = delegatedValue
-        set(valToSet) {
-            setter(valToSet)
-            delegatedValue = valToSet
-        }
-
-    override fun component1() = value
-
-    override fun component2(): (T) -> Unit = this::value.setter
-}
+inline infix fun <T> MutableState<T>.withSetter(crossinline setter: (newValue: T) -> Unit) =
+    object : MutableState<T> by this {
+        override var value
+            get() = this@withSetter.value
+            set(valToSet) {
+                setter(valToSet)
+                this@withSetter.value = valToSet
+            }
+    }
