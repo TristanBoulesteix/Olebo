fc62ac36282db677a3fdd023e8e0f35bfca685aa
Tristan <tristan.boulesteix@gmail.com>
2021-09-21 23:20:32 +0200

    Update Application.kt

--- Website/src/main/kotlin/Application.kt
+++ Website/src/main/kotlin/Application.kt
@@ -6,8 +6,15 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", watchPaths = listOf("Website", "Update")) {
-        configureRouting()
-        configureSerialization()
-    }.start(wait = true)
+    val environment = applicationEngineEnvironment {
+        connector {
+            port = 8080
+        }
+        module {
+            configureRouting()
+            configureSerialization()
+        }
+    }
+
+    embeddedServer(Netty, environment).start(wait = true)
 }
