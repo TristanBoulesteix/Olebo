a0afbfce7e509152e44df628c7ba37a56393f7b5
Tristan <tristan.boulesteix@gmail.com>
2021-11-14 17:55:14 +0100

    Disable https code temporarily for tests

--- Website/src/jvmMain/kotlin/Application.kt
+++ Website/src/jvmMain/kotlin/Application.kt
@@ -2,27 +2,23 @@ package fr.olebo
 
 import fr.olebo.plugins.configureRouting
 import fr.olebo.plugins.configureSerialization
-import fr.olebo.plugins.httpsRedirect
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import java.io.File
-import java.security.KeyStore
-import java.util.*
 
 fun main() {
-    val prop = Properties().apply {
+    /*val prop = Properties().apply {
         File("/var/opt/olebo/config.properties").inputStream().use(this::load)
     }
 
     val pwd: String by prop
     val keyStoreFile = File("/etc/letsencrypt/live/olebo.fr/keystore.jks")
-    val keyStore: KeyStore = KeyStore.getInstance(keyStoreFile, pwd.toCharArray())
+    val keyStore: KeyStore = KeyStore.getInstance(keyStoreFile, pwd.toCharArray())*/
 
     val environment = applicationEngineEnvironment {
         connector {
             port = 8080
         }
-        sslConnector(
+        /*sslConnector(
             keyStore = keyStore,
             keyAlias = "olebo",
             keyStorePassword = pwd::toCharArray,
@@ -30,9 +26,9 @@ fun main() {
         ) {
             port = 8443
             keyStorePath = keyStoreFile
-        }
+        }*/
         module {
-            httpsRedirect()
+            //httpsRedirect()
             configureRouting()
             configureSerialization()
         }
