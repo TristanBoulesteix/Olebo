712f741073f2bc57f274bd1f141fb88fac129b77
Tristan <tristan.boulesteix@gmail.com>
2021-11-18 22:58:24 +0100

    Update ShareSceneRouting.kt

--- Website/src/jvmMain/kotlin/sharescene/ShareSceneRouting.kt
+++ Website/src/jvmMain/kotlin/sharescene/ShareSceneRouting.kt
@@ -1,18 +1,14 @@
 package fr.olebo.sharescene
 
-import io.ktor.application.*
 import io.ktor.http.cio.websocket.*
-import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.websocket.*
 
+private const val PARAM_NAME = "sceneId"
+
 fun Routing.shareSceneRouting() {
-    route("scene") {
-        get {
-            call.respondText("Hello World!")
-        }
-        webSocket("echo") {
-            send(Frame.Text("Enter your name"))
-        }
+    webSocket("scene/{$PARAM_NAME}") {
+        val sceneId = call.parameters[PARAM_NAME] ?: return@webSocket
+        send(Frame.Text("Connected with id $sceneId"))
     }
 }
