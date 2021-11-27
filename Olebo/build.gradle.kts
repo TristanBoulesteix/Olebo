b9ecfafd613b40277a5a493f3b58935f56258ee1
Tristan <tristan.boulesteix@gmail.com>
2021-11-27 15:20:00 +0100

    Upgrade compose version

--- Olebo/build.gradle.kts
+++ Olebo/build.gradle.kts
@@ -7,7 +7,7 @@ val exposedVersion: String by project.parent!!
 
 plugins {
     kotlin("multiplatform")
-    id("org.jetbrains.compose") version "1.0.0-beta6-dev474"
+    id("org.jetbrains.compose") version "1.0.0-rc5"
     id("org.sonarqube") version "3.3"
 }
 
