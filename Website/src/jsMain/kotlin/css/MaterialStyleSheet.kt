157dabb94a05fecd1737eb8354ba6bbde92b7115
Tristan <tristan.boulesteix@gmail.com>
2021-12-07 09:26:04 +0100

    Fix material stylesheet

--- Website/src/jsMain/kotlin/css/MaterialStyleSheet.kt
+++ Website/src/jsMain/kotlin/css/MaterialStyleSheet.kt
@@ -1,15 +1,20 @@
 package fr.olebo.sharescene.css
 
 import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.dom.stringPresentation
 
 object MaterialStyleSheet : StyleSheet() {
     val materialTextField by style {
         display(DisplayStyle.InlineBlock)
         overflow("hidden")
+        position(Position.Relative)
+        lineHeight("1.5")
 
         child(self, type("input")) style {
             display(DisplayStyle.Block)
             boxSizing("border-box")
+            margin(zero)
+            border(style = LineStyle.None)
             border(location = BorderLocation.Top, style = LineStyle.Solid, width = 27.px, color = Color.transparent)
             border(location = BorderLocation.Bottom, style = LineStyle.Solid, width = 1.px, color = rgbaMaterial(.6))
             padding(zero, zero, 4.px)
@@ -17,11 +22,11 @@ object MaterialStyleSheet : StyleSheet() {
             color(rgbaMaterial(.87))
             backgroundColor(Color.transparent)
             property("box-shadow", "none")
-            property("caret-color", "rgb(var(--pure-material-primary-rgb, 33, 150, 243))")
+            property("caret-color", rgbMaterialPrimary())
             property("transition", "border-bottom 0.2s, background-color 0.2s")
         }
 
-        child(self, type("input") + type("span")) style {
+        child(self, adjacent(type("input"), type("span"))) style {
             position(Position.Absolute)
             top(zero)
             left(zero)
@@ -37,9 +42,60 @@ object MaterialStyleSheet : StyleSheet() {
             property("transition", "color 0.2s, font-size 0.2s, line-height 0.2s")
         }
 
-        child(self, type("input") + hover())
+        child(self, type("input") + after) style {
+            property("content", "")
+            position(Position.Absolute)
+            left(zero)
+            bottom(zero)
+            display(DisplayStyle.Block)
+            width(100.percent)
+            height(2.px)
+            backgroundColor(rgbMaterialPrimary())
+            property("transform-origin", "bottom center")
+            property("transform", "scaleX(0)")
+            property("transition", "transform 0.2s")
+        }
+
+        child(self, hover(type("input"))) style {
+            property("border-bottom-color", rgbaMaterial(.87))
+        }
+
+        child(self, adjacent(type("input") + not(focus) + placeholderShown, type("span"))) style {
+            lineHeight(56.px)
+        }
+
+        child(self, type("input") + focus) style {
+            outline("none")
+        }
+
+        child(self, adjacent(type("input") + focus, type("span"))) style {
+            color(rgbMaterialPrimary())
+        }
+
+        child(self, adjacent(type("input") + focus, type("span") + before)) style {
+            opacity(.12)
+        }
+
+        child(self, adjacent(type("input") + focus, type("span") + after)) style {
+            property("transform", "scale(1)")
+        }
+
+        child(self, type("input") + disabled) style {
+            property("border-bottom-color", rgbaMaterial(.38))
+            color(rgbaMaterial(.38))
+        }
+
+        child(self, adjacent(type("input") + disabled, type("span"))) style {
+            color(rgbaMaterial(.38))
+        }
     }
 
     private fun rgbaMaterial(opacity: Number) =
         "rgba(var(--pure-material-onsurface-rgb, 0, 0, 0), $opacity)".unsafeCast<CSSColorValue>()
+
+    private fun rgbMaterialPrimary() = "rgb(var(--pure-material-primary-rgb, 33, 150, 243))".unsafeCast<CSSColorValue>()
+
+    init {
+        println(cssRules.joinToString(separator = "\n") { it.stringPresentation() })
+    }
 }
