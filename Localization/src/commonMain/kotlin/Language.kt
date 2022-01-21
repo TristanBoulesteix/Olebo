419fb682e7b3a1f21435b14904c2439f71405003
Tristan <tristan.boulesteix@gmail.com>
2022-01-21 11:19:18 +0100

    Update Language.kt

--- Localization/src/commonMain/kotlin/Language.kt
+++ Localization/src/commonMain/kotlin/Language.kt
@@ -2,6 +2,7 @@ package jdr.exia.localization
 
 expect class Locale
 
+@Suppress("unused") // Suppress unused. This warning is a bug from the Kotlin Multiplatform plugin.
 expect value class Language(val locale: Locale) {
     companion object {
         internal val english: Language
