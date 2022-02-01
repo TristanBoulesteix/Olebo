26ca34cd1d2b2596ae70fba1cbbc98205ade0301
raspberry-server <olebo@raspberry-server.com>
2022-02-01 15:41:39 +0100

    Fix path for script

--- prepare_server_config.sh
+++ prepare_server_config.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cp /var/opt/olebo/config.properties ./Website/src/jvmMain/ressources/config.properties
+cp /var/opt/olebo/config.properties Website/src/jvmMain/resources/config.properties
