91ee3ddd8fca2fbadacc10e331c4e2601690a15d
Tristan <tristan.boulesteix@gmail.com>
2022-02-02 18:49:14 +0100

    remove Hello world

--- Website/src/jvmMain/kotlin/plugins/Routing.kt
+++ Website/src/jvmMain/kotlin/plugins/Routing.kt
@@ -4,16 +4,12 @@ import fr.olebo.sharescene.shareSceneRouting
 import fr.olebo.update.releaseRouting
 import io.ktor.application.*
 import io.ktor.http.content.*
-import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Application.configureRouting() {
     routing {
         static("/static") {
             resources()
-        }
-        get("/") {
-            call.respondText("Hello World!")
         }
         releaseRouting()
         shareSceneRouting()
