55bb396db3b98b359dc9146c3002f6888469dde4
Tristan <tristan.boulesteix@gmail.com>
2022-02-02 19:30:04 +0100

    fix config script

--- prepare_server_config.sh
+++ prepare_server_config.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cd /hone/tristan/Olebo || exit
 cp /var/opt/olebo/config.properties Website/src/jvmMain/resources/config.properties
 cp /etc/letsencrypt/live/olebo.fr-0001/keystore.jks Website/src/jvmMain/resources/keystore.jks
 ./gradlew :Website:installDist
