66cae251d5740e47f099c00fa33bb57e117241e7
Tristan <tristan.boulesteix@gmail.com>
2022-02-03 15:44:31 +0100

    Update prepare_server_config.sh

--- prepare_server_config.sh
+++ prepare_server_config.sh
@@ -4,5 +4,5 @@ cp /var/opt/olebo/config.properties Website/src/jvmMain/resources/config.propert
 cp /etc/letsencrypt/live/olebo.fr-0001/keystore.jks Website/src/jvmMain/resources/keystore.jks
 ./gradlew :Website:installDist
 docker build -t olebo-website .
-docker stop olebo-website
-docker run -p 8080:8080 -p 8443:8443 -v /home/tristan/Olebo_releases:/app/bin/releases olebo-website
+docker stop website
+docker run -p 8080:8080 -p 8443:8443 -v /home/tristan/Olebo_releases:/app/bin/releases --name website olebo-website
