ac9387f640d6c11172a50563889f025d41b69f04
Tristan <tristan.boulesteix@gmail.com>
2023-02-26 14:51:48 +0100

    Add steps for form

---
+++ Olebo/src/jvmMain/kotlin/view/component/form/StepsIndicator.kt
@@ -0,0 +1,32 @@
+package jdr.exia.view.component.form
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun StepsIndicator(numberOfSteps: Int, currentStep: Int, modifier: Modifier = Modifier) = Row(
+    horizontalArrangement = Arrangement.Center,
+    modifier = Modifier.fillMaxWidth() then modifier
+) {
+    for (i in 1..numberOfSteps) {
+        Step(i, i == currentStep)
+        Spacer(Modifier.width(16.dp))
+    }
+}
+
+@Composable
+private fun Step(index: Int, isSelected: Boolean) = Box(
+    modifier = Modifier.size(24.dp).clip(CircleShape)
+        .background(if (isSelected) MaterialTheme.colors.primary else MaterialTheme.colors.onSurface.copy(alpha = .12f)),
+    contentAlignment = Alignment.Center
+) {
+    Text(index.toString())
+}
--- Olebo/src/jvmMain/kotlin/view/composable/editor/act/ActCreator.kt
+++ Olebo/src/jvmMain/kotlin/view/composable/editor/act/ActCreator.kt
@@ -1,27 +1,44 @@
 package jdr.exia.view.composable.editor.act
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import jdr.exia.localization.STR_NEW_ACT
 import jdr.exia.localization.StringLocale
 import jdr.exia.localization.get
+import jdr.exia.model.tools.success
+import jdr.exia.view.component.FooterRowWithCancel
 import jdr.exia.view.component.HeaderRow
+import jdr.exia.view.component.form.StepsIndicator
+import jdr.exia.view.tools.BorderBuilder
+import jdr.exia.view.tools.toBorderStroke
+import jdr.exia.view.ui.roundedTopShape
 
 @Composable
 fun ActCreator(onDone: () -> Unit) = Column {
     Header()
 
     Column(modifier = Modifier.fillMaxSize().background(MaterialTheme.colors.secondaryVariant).padding(15.dp)) {
-        Text("test")
+        Card(
+            modifier = Modifier.padding(top = 20.dp, end = 20.dp, start = 20.dp).fillMaxWidth(),
+            border = BorderBuilder.defaultBorder.toBorderStroke(),
+            shape = roundedTopShape
+        ) {
+            Text("test")
+        }
+
+        FooterRowWithCancel(
+            onDone = onDone,
+            onConfirm = { Result.success }
+        )
     }
 }
 
@@ -29,7 +46,11 @@ fun ActCreator(onDone: () -> Unit) = Column {
 private fun Header() {
     HeaderRow {
         Surface(color = Color.Transparent, contentColor = MaterialTheme.colors.onSurface) {
-            Text(StringLocale[STR_NEW_ACT])
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                StepsIndicator(3, 1)
+                Spacer(Modifier.height(2.dp))
+                Text(StringLocale[STR_NEW_ACT])
+            }
         }
     }
 }
