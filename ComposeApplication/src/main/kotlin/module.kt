d496f84dafa17b1d2e134f9603f2722808d0fee8
Tristan <tristan.boulesteix@gmail.com>
2024-02-18 20:18:39 +0100

    ♻️ Add path to Olebo directory

--- ComposeApplication/src/main/kotlin/module.kt
+++ ComposeApplication/src/main/kotlin/module.kt
@@ -1,17 +1,23 @@
 package fr.olebo
 
+import fr.olebo.domain.adaptors.system.OsAdaptor
 import fr.olebo.domain.domainModule
 import fr.olebo.persistence.persistenceModule
 import fr.olebo.system.systemModule
 import org.kodein.di.DI
+import org.kodein.di.bindSingleton
+import org.kodein.di.instance
+import java.io.File
 
 private val module by DI.Module {
-
+    bindSingleton("olebo-directory") {
+        "${instance<OsAdaptor>().current.appDataDir}${File.separator}Olebo${File.separator}"
+    }
 }
 
 val injector = DI {
-    import(module)
-    import(domainModule)
     import(systemModule)
+    import(domainModule)
     import(persistenceModule)
+    import(module)
 }
