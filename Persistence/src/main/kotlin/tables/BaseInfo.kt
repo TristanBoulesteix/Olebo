17810c89b0bc87841fd2f9dbdabe122524c6dfe4
Tristan <tristan.boulesteix@gmail.com>
2024-02-25 12:56:09 +0100

    ✅ BaseInfo initialization - GREEN

--- Persistence/src/main/kotlin/tables/BaseInfo.kt
+++ Persistence/src/main/kotlin/tables/BaseInfo.kt
@@ -1,10 +1,13 @@
 package fr.olebo.persistence.tables
 
+import fr.olebo.domain.model.system.OleboConfiguration
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.upsert
 import org.kodein.di.DI
 import org.kodein.di.DIAware
+import org.kodein.di.instance
 
 internal class BaseInfo(override val di: DI) : Table(), Initializable, DIAware {
     private val keyInfo = varchar("key_info", 50)
@@ -14,10 +17,15 @@ internal class BaseInfo(override val di: DI) : Table(), Initializable, DIAware {
     override val primaryKey = PrimaryKey(keyInfo)
 
     val versionBase
-        get() = transaction { select(value).where(keyInfo eq BASE_VERSION).firstOrNull()?.get(value)?.toIntOrNull() }
+        get() = transaction { select(value).where(keyInfo eq BASE_VERSION).lastOrNull()?.get(value)?.toIntOrNull() }
 
-    override fun initialize() {
-        TODO("Not yet implemented")
+    override fun initialize(): Unit = transaction {
+        upsert {
+            val configuration by instance<OleboConfiguration>()
+
+            it[keyInfo] = BASE_VERSION
+            it[value] = configuration.versionCode.toString()
+        }
     }
 
     companion object {
