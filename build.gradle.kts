e8a6748bcb3334ddce17b1d8c9731d3f40d4763e
Tristan <tristan.boulesteix@gmail.com>
2024-03-03 17:47:48 +0100

    Set up kover

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,14 @@
+import kotlinx.kover.gradle.plugin.KoverGradlePlugin
+
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.mock) apply false
+    alias(libs.plugins.kover) apply false
 }
 
 subprojects {
-    afterEvaluate {
-        when {
-            plugins.hasPlugin("java") -> {
-                tasks.register<Test>("allTests") {
-                    description = "Run JVM tests"
-                    group = "Tests"
-                    useJUnitPlatform()
-                }
-            }
-        }
-    }
+    apply<KoverGradlePlugin>()
 }
