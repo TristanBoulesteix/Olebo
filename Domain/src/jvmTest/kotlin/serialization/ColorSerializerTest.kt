3381272d104c9952819244d44e1d00f984ba8136
Tristan <tristan.boulesteix@gmail.com>
2024-03-12 20:43:10 +0100

    ♻️ Refactor test

--- Domain/src/jvmTest/kotlin/serialization/ColorSerializerTest.kt
+++ Domain/src/jvmTest/kotlin/serialization/ColorSerializerTest.kt
@@ -2,19 +2,28 @@ package fr.olebo.domain.tests.serialization
 
 import androidx.compose.ui.graphics.Color
 import fr.olebo.domain.serialization.ColorSerializer
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.json.Json
+import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-class ColorSerializerTest {
+internal class ColorSerializerTest {
+    private lateinit var serializer: KSerializer<Color>
+
+    @BeforeTest
+    fun initialize() {
+        serializer = ColorSerializer()
+    }
+
     @Test
     fun `serialize a color`() {
         val color = Color.Blue
 
-        val stringColor = Json.encodeToString(ColorSerializer(), color)
+        val stringColor = Json.encodeToString(serializer, color)
         assertEquals("\"-16776961\"", stringColor)
 
-        val actualColor = Json.decodeFromString(ColorSerializer(), stringColor)
+        val actualColor = Json.decodeFromString(serializer, stringColor)
         assertEquals(color, actualColor)
     }
 }
